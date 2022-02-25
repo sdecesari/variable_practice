b49e01718572eb70cc1506adf517416817e5356b
Shirley DeCesari <shirley.decesari@gmail.com>
2022-02-24 21:34:17 -0700

    Update variable

---
+++ variables.rb
@@ -0,0 +1,14 @@
+# variable name is assigned to the string 'Shirley'
+name = 'Shirley'
+dog_name = 'Asher'
+age = 35
+kids = 0
+# variable shoe_size is assigned to the float 5.5
+shoe_size = 5.5
+dog_age = 0.8
+# varaible dog_asleep is assigned to the boolean true
+dog_asleep = false
+dinner_made = false
+age = 36
+# varaible dog_asleep is reassigned to the boolean false
+dog_asleep = true
